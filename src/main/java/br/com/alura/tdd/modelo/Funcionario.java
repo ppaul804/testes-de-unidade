04c5e134a04ecc3cfcc18728d344572988ab8316
Pedro Paul <pedro.paul@cassi.com.br>
2023-11-19 16:56:56 -0300

    Refatoração com lombok e Retirada de Calculadora

--- src/main/java/br/com/alura/tdd/modelo/Funcionario.java
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -1,30 +1,15 @@
 package br.com.alura.tdd.modelo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
 public class Funcionario {
-
-	private String nome;
-	private LocalDate dataAdmissao;
-	private BigDecimal salario;
-
-	public Funcionario(String nome, LocalDate dataAdmissao, BigDecimal salario) {
-		this.nome = nome;
-		this.dataAdmissao = dataAdmissao;
-		this.salario = salario;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public LocalDate getDataAdmissao() {
-		return dataAdmissao;
-	}
-
-	public BigDecimal getSalario() {
-		return salario;
-	}
-
+    private String nome;
+    private LocalDate dataAdmissao;
+    private BigDecimal salario;
 }
--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -6,12 +6,11 @@ import br.com.alura.tdd.modelo.Funcionario;
 
 public class BonusService {
 
-	public BigDecimal calcularBonus(Funcionario funcionario) {
-		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
-		if (valor.compareTo(new BigDecimal("1000")) > 0) {
-			valor = BigDecimal.ZERO;
-		}
-		return valor;
-	}
+    public BigDecimal calcularBonus(Funcionario funcionario) {
+        BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+        if (valor.compareTo(new BigDecimal("1000")) > 0)
+            return BigDecimal.ZERO;
+        return valor;
+    }
 
 }
